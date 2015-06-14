cd08ee2cedf20602041e4c2ae216422b89f1f8cc
Elena Ivanova <eoivanova@abv.bg>
2015-06-14 19:36:27 +0300

    Test

--- test/network_spec.rb
+++ test/network_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/tripfinder'
 
 describe Network do
   before do
-    Tripfinder.configure({:points => "test/points_small.txt", :routes =>"test/routes_small.txt"})	  
+    Tripfinder.configure({:points => "test/points_small.txt", :routes =>"test/routes_small.txt"})
     @network = Network.new #("test/points_small.txt", "test/routes_small.txt")
   end
 
@@ -10,18 +10,18 @@ describe Network do
     it "should load the correct number of point from file" do
       expect(@network.size).to eql 4
     end
-    
+
     it "should load expected points from file" do
       point = @network.find_by_name("хижа Безбог")
       expect(point).not_to be_nil
       expect(point).to be_instance_of Point
       expect(point.name).to eql "хижа Безбог"
       expect(point.region).to eql "Пирин"
-      expect(point.starting_point).to eql true 
+      expect(point.starting_point).to eql true
       expect(point.coordinates).to eql "41.73434 23.52475"
       expect(point.altitude).to eql "2236"
       expect(point.type).to eql "хижа"
-      expect(point.comments).to be_empty 
+      expect(point.comments).to be_empty
     end
   end
 
@@ -29,7 +29,8 @@ describe Network do
     it "should return a collection of defined and implicit reverse paths" do
       point = @network.find_by_name("връх Безбог")
       paths = @network.paths_from(point)
-      expect(paths.size).to eql 2 
+      expect(paths.collect {|path| path.hours}.include? 2 )
+      expect(paths.size).to eql 2
     end
   end
 end
@@ -43,9 +44,9 @@ describe Finder do
   describe "#find" do
     it "should return prioritized list of routes" do
       params = {:days => 1 , :hours => 6, :cyclic => false}
-      routes = @finder.find(params)	    
+      routes = @finder.find(params)
       expect(routes).not_to be nil
-      expect(routes.size).to be 5 
+      expect(routes.size).to be 5
     end
 
     it "should respond to strig parameters" do
