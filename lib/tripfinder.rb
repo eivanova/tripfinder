96e68ec01f62845951c0776c3372d4d721fa08b3
Elena Ivanova <eoivanova@abv.bg>
2015-07-25 15:02:57 +0300

    Require eliza in gem

--- lib/tripfinder.rb
+++ lib/tripfinder.rb
@@ -1,8 +1,9 @@
 require 'tripfinder/domain'
 require 'tripfinder/filters'
+require 'chatbot/eliza'
 
 class Finder
-  
+
   def initialize(network)
     @network = network
     @chain = initialize_chain()
@@ -14,15 +15,15 @@ class Finder
     chain
   end
 
-  # search happens by two types of characteristics - those of single 
-  # point/path, which are handled by the chain of filters by applying 
+  # search happens by two types of characteristics - those of single
+  # point/path, which are handled by the chain of filters by applying
   # weight masks; and those of route construction, like number of days,
   # lenght of each day, etc... Those are handled by the collect_routes
   # method
   def find(params)
     mask = init_mask
     for filter in @chain
-      mask = filter.apply_mask(@network, mask, params)	    
+      mask = filter.apply_mask(@network, mask, params)
     end
 
     collect_routes(mask, params)
@@ -36,18 +37,18 @@ class Finder
     starting_points = @network.points.select { |point| point.starting_point }
     # TODO do the search for the top n points only? What if we have had a filter
     # for region, do we still search points from other regions? My guess for now
-    # is yes 
+    # is yes
 
     # A basic search - for each starting point, see if we can get to
-    # a starting point again in the necessary number of hops. If yes, save the 
+    # a starting point again in the necessary number of hops. If yes, save the
     # route to routes; if not - proceed to the next starting point. This could be
     # slow, will see and optimise if necessary.
     for start  in starting_points
       weighted = get_weighted_routes(start, params, mask)
-      routes.merge! weighted    
+      routes.merge! weighted
     end
 
-    routes    
+    routes
   end
 
   # Get the routes starting at start and adhering the paramteres in params.
@@ -57,19 +58,19 @@ class Finder
     routes = {}
     current_route = RouteBuilder.new(params[:hours].to_i, params[:days].to_i, params[:cyclic])
     current_weight = 0.1
-    weighted_routes(start, routes, current_route, current_weight, mask) 
+    weighted_routes(start, routes, current_route, current_weight, mask)
     routes
   end
 
   def weighted_routes(start, routes, current_route, current_weight, mask)
     for path in @network.paths_from(start)
-      next if path.hours > current_route.hours_per_day or current_route.contains_path path 
+      next if path.hours > current_route.hours_per_day or current_route.contains_path path
       this_route = current_route.new_route
       this_weight = current_weight * mask[path]
       done, this_route = this_route.add_path path
-      if done == true	      
+      if done == true
         routes[this_route.build] = this_weight
-      elsif done == 1 
+      elsif done == 1
         start = this_route.finish
         weighted_routes(start, routes, this_route, this_weight, mask)
       end
